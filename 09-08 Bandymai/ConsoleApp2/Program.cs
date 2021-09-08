ec7bbfe38bdee8c507d5933da2254f1c67146689
jonaspetraska7 <60759096+jonaspetraska7@users.noreply.github.com>
2021-09-08 21:25:12 +0300

    Struktura

---
+++ 09-08 Bandymai/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Suma(1, 20);
+        }
+        static void Suma(int x, int y)
+        {
+            Console.WriteLine(x+y);
+        }
+    }
+}
