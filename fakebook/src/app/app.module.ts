185a71099b5c21ca054d067dc1faaa1a322a12b1
PauliusCiganas <89936529+PauliusCiganas@users.noreply.github.com>
2021-10-14 19:51:03 +0300

    *kairinisMeniu
    *sukurtas kairinio meniu komponentas

--- fakebook/src/app/app.module.ts
+++ fakebook/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 
 import { MeniuComponent } from './komponentai/meniu/meniu.component';
 import { PagrindinispuslapisComponent } from './puslapiai/pagrindinispuslapis/pagrindinispuslapis.component';
+import { KairinisMeniuComponent } from './komponentai/kairinis-meniu/kairinis-meniu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
 
     MeniuComponent,
-    PagrindinispuslapisComponent
+    PagrindinispuslapisComponent,
+    KairinisMeniuComponent
   ],
   imports: [
     BrowserModule,
---
+++ fakebook/src/app/komponentai/kairinis-meniu/kairinis-meniu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kairinis-meniu',
+  templateUrl: './kairinis-meniu.component.html',
+  styleUrls: ['./kairinis-meniu.component.css']
+})
+export class KairinisMeniuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
