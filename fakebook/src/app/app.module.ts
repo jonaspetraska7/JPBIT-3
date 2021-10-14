c779c3ebf5966f30d42ab98456e92026c22a6e81
NeoEvaldas <38045668+NeoEvaldas@users.noreply.github.com>
2021-10-14 19:51:39 +0300

    pridetas komponentas

--- fakebook/src/app/app.module.ts
+++ fakebook/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 
 import { MeniuComponent } from './komponentai/meniu/meniu.component';
 import { PagrindinispuslapisComponent } from './puslapiai/pagrindinispuslapis/pagrindinispuslapis.component';
+import { StoryMeniuComponent } from './komponentai/story-meniu/story-meniu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
 
     MeniuComponent,
-    PagrindinispuslapisComponent
+    PagrindinispuslapisComponent,
+    StoryMeniuComponent
   ],
   imports: [
     BrowserModule,
