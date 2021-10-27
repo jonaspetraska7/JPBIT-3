26a6778bd997e6cd93f0d65d286386f5c9f7a9ab
MigleGost <89936436+MigleGost@users.noreply.github.com>
2021-10-27 21:56:08 +0300

    Sukuriau firebase servisa
    -Sukuriau firebase.service
    -Is anksti surasiau komentarus kiekvienai  serviso daliai

---
+++ fakebook/src/app/firebase.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  constructor() { }
+
+
+
+//draugu sarasa *Gavimas*
+
+
+//feedas *Gavimas*
+
+
+//zinutes-ivedimas *Siuntimas*
+
+
+//profilis *Gavimas*
+
+
+//login *Auth Modulis*
+
+
+//register *Auth Modulis*
+
+
+//register *Siuntimas user info i DB* (Jei spesim)
+
+
+
+
+
+}
