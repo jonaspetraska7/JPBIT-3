f39225fd61a1ecfef5ae171e0e576f73329cd814
PauliusCiganas <89936529+PauliusCiganas@users.noreply.github.com>
2021-10-28 20:42:45 +0300

    login pataisymai

--- fakebook/src/app/komponentai/login/login.component.ts
+++ fakebook/src/app/komponentai/login/login.component.ts
@@ -16,7 +16,7 @@ export class LoginComponent implements OnInit {
   }
 
   login(){
-    this.auth.login('mauglis','nugaletojas');
+    this.auth.login('mauglis@mauglis.lt','mauglis');
   }
 
 popup = false;
