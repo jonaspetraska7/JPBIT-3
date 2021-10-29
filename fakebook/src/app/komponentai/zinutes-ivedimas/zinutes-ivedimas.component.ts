aa2bac78eb8f2fc35c587c26d72bddef00cfa2a1
reikale <sviackeviciute@gmail.com>
2021-10-29 19:53:28 +0300

    Update zinutes-ivedimas.component.ts

--- fakebook/src/app/komponentai/zinutes-ivedimas/zinutes-ivedimas.component.ts
+++ fakebook/src/app/komponentai/zinutes-ivedimas/zinutes-ivedimas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { InteraktyvumasService } from 'src/app/interaktyvumas.service';
+import { FirebaseService } from 'src/app/firebase.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-zinutes-ivedimas',
@@ -8,7 +10,9 @@ import { InteraktyvumasService } from 'src/app/interaktyvumas.service';
 })
 export class ZinutesIvedimasComponent implements OnInit {
 
-  constructor(private _interaktyvumasService: InteraktyvumasService) { }
+  constructor(
+    private _interaktyvumasService: InteraktyvumasService,
+    private firestore: AngularFirestore) { }
 
   getVal(val: any)
   {
@@ -17,6 +21,12 @@ export class ZinutesIvedimasComponent implements OnInit {
     this._interaktyvumasService.sendMessage(val.target.value);
   }
 
+  addPost(val: any){
+    let irasiukas = {
+      turinys: val
+    }
+    this.firestore.collection('Irasai').add(irasiukas);
+  }
 
   ngOnInit(): void {
   }
