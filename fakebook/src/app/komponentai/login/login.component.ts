625c5658adcecb6e51bd3d56b88304571ae87216
NeoEvaldas <neoevaldas@gmail.com>
2021-10-29 12:48:43 +0300

    Revert "login pataisymai"
    This reverts commit f39225fd61a1ecfef5ae171e0e576f73329cd814.

--- fakebook/src/app/komponentai/login/login.component.ts
+++ fakebook/src/app/komponentai/login/login.component.ts
@@ -16,7 +16,7 @@ export class LoginComponent implements OnInit {
   }
 
   login(){
-    this.auth.login('mauglis@mauglis.lt','mauglis');
+    this.auth.login('mauglis','nugaletojas');
   }
 
 popup = false;
