450603bb28a64f203e7c71a81021ed189549704c
NeoEvaldas <neoevaldas@gmail.com>
2021-10-29 12:47:10 +0300

    Revert "profilio puslapio update"
    This reverts commit d92a984fea951059a1985086de124acda18fcb58.

--- fakebook/src/app/komponentai/profilio-nuotraukos/profilio-nuotraukos.component.ts
+++ fakebook/src/app/komponentai/profilio-nuotraukos/profilio-nuotraukos.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-profilio-nuotraukos',
@@ -7,36 +6,10 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
   styleUrls: ['./profilio-nuotraukos.component.css']
 })
 export class ProfilioNuotraukosComponent implements OnInit {
-  atsakymas!: string;
-  nuotrauku_sarasas : any[] = [];
 
+  constructor() { }
 
-  constructor( private firestore: AngularFirestore) {
-    this.firestore.collection('Profilis').valueChanges().subscribe((x : any) => 
-    this.atsakymas = x[0]
-    
-    );
-    
-   }
-  
   ngOnInit(): void {
-    window.setTimeout( () => {
-      this.isrikiuoti();
-      
-    }, 1000);
   }
- 
-  isrikiuoti(){
-    console.log(this.atsakymas);
-    for (const [key, value] of Object.entries(this.atsakymas)) {
-      console.log(key, value);
-      this.nuotrauku_sarasas.push(value);
-      console.log(this.nuotrauku_sarasas);
-      
-    }
-      
-    }
-    
-   
-  
+
 }
