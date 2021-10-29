55e83e18366c5dc1a2cbf510acd0bb9544fae510
jonaspetraska7 <60759096+jonaspetraska7@users.noreply.github.com>
2021-10-29 18:10:43 +0300

    Revert "Revert "profilio puslapio update""
    This reverts commit 450603bb28a64f203e7c71a81021ed189549704c.

--- fakebook/src/app/komponentai/profilio-nuotraukos/profilio-nuotraukos.component.ts
+++ fakebook/src/app/komponentai/profilio-nuotraukos/profilio-nuotraukos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-profilio-nuotraukos',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profilio-nuotraukos.component.css']
 })
 export class ProfilioNuotraukosComponent implements OnInit {
+  atsakymas!: string;
+  nuotrauku_sarasas : any[] = [];
 
-  constructor() { }
 
+  constructor( private firestore: AngularFirestore) {
+    this.firestore.collection('Profilis').valueChanges().subscribe((x : any) => 
+    this.atsakymas = x[0]
+    
+    );
+    
+   }
+  
   ngOnInit(): void {
+    window.setTimeout( () => {
+      this.isrikiuoti();
+      
+    }, 1000);
   }
-
+ 
+  isrikiuoti(){
+    console.log(this.atsakymas);
+    for (const [key, value] of Object.entries(this.atsakymas)) {
+      console.log(key, value);
+      this.nuotrauku_sarasas.push(value);
+      console.log(this.nuotrauku_sarasas);
+      
+    }
+      
+    }
+    
+   
+  
 }
