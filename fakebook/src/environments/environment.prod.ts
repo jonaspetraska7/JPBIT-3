3ac479781d364c7db5c68ff226fad97ef9d16322
PauliusCiganas <89936529+PauliusCiganas@users.noreply.github.com>
2021-11-03 20:41:52 +0200

    pakeitimas firebase

--- fakebook/src/environments/environment.prod.ts
+++ fakebook/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: "AIzaSyB5JL1M_MJF-JXQDnyvVv_i_eSDM_Qdy1M",
+    authDomain: "jpbit-3.firebaseapp.com",
+    databaseURL: "https://jpbit-3-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "jpbit-3",
+    storageBucket: "jpbit-3.appspot.com",
+    messagingSenderId: "137499587705",
+    appId: "1:137499587705:web:3743918ef051df266ed0a8"
+  }
   };
+  
